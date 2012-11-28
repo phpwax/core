66db9ca90b5cc73eeab0be3b61fdfac9a0728022
Ross Riley <riley.ross@gmail.com>
2012-11-28 16:17:19 +0000

    no dynamic namespaceing

--- DefaultRoutes.php
+++ DefaultRoutes.php
@@ -3,15 +3,18 @@ namespace Wax\Core;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
+$reflector = new \ReflectionClass('');
+var_dump($reflector->getNamespaceName());
+
 $collection = new RouteCollection();
 $collection->add(
-  'default_home', new Route('/', ['controller' => __NAMESPACE__.'\Controller\PageController'])
+  'default_home', new Route('/', ['controller' => 'Controller\PageController'])
 );
 
 $collection->add(
   'default_action', 
   new Route('/{action}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       "_format"=>"html"
     ],
@@ -21,7 +24,7 @@ $collection->add(
 $collection->add(
   'default_action_id', 
   new Route('/{action}/{id}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       'id'          =>false,
       "_format"=>"html"
@@ -32,7 +35,7 @@ $collection->add(
 $collection->add(
   'default_action_id_params', 
   new Route('/{action}/{id}/{params}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       'id'          =>false,
       "params"      =>false,
