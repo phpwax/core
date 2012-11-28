d1d8ee8393128b056df53f8022d0f64cfb91f151
Ross Riley <riley.ross@gmail.com>
2012-11-28 14:44:01 +0000

    protected

--- DefaultRoutes.php
+++ DefaultRoutes.php
@@ -10,10 +10,10 @@ $collection->add(
 
 $collection->add(
   'default_action_id_params', 
-  new Route('/{action}/{id}/{params}.{_format}', 
+  new Route('/{_action}/{_id}/{params}.{_format}', 
     [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
-      'action'      =>"index",
-      'id'          =>false,
+      '_action'      =>"index",
+      '_id'          =>false,
       "params"      =>false,
       "_format"=>"html"
     ],
