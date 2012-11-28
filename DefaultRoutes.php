578271d792510e6de7637ba5de7bca58c1b0c252
Ross Riley <riley.ross@gmail.com>
2012-11-28 16:34:55 +0000

    fix

--- DefaultRoutes.php
+++ DefaultRoutes.php
@@ -3,8 +3,7 @@ namespace Wax\Core;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
-$reflector = new \ReflectionClass('');
-var_dump($reflector->getNamespaceName());
+
 
 $collection = new RouteCollection();
 $collection->add(
@@ -14,7 +13,7 @@ $collection->add(
 $collection->add(
   'default_action', 
   new Route('/{action}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       "_format"=>"html"
     ],
@@ -24,7 +23,7 @@ $collection->add(
 $collection->add(
   'default_action_id', 
   new Route('/{action}/{id}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       'id'          =>false,
       "_format"=>"html"
@@ -35,7 +34,7 @@ $collection->add(
 $collection->add(
   'default_action_id_params', 
   new Route('/{action}/{id}/{params}.{_format}', 
-    [ 'controller'  => __NAMESPACE__.'\Controller\PageController', 
+    [ 'controller'  => 'Controller\PageController', 
       'action'      =>"index",
       'id'          =>false,
       "params"      =>false,
